ef87771d7e7278267c8074a87dc1748dd5938f78
Lazy_V <954294627@qq.com>
2026-03-10 04:55:43 +0000

    fix: use newer slang that fixed shader-slang/slang#6668

--- source/vshadersystem/xmake.lua
+++ source/vshadersystem/xmake.lua
@@ -1,8 +1,8 @@
--- note: spirv-cross & glslang & slang must require the same vulkan sdk version
+-- note: spirv-cross & glslang must require the same vulkan sdk version
 -- we use 1.4.309
 add_requires("spirv-cross vulkan-sdk-1.4.309", {configs = { shared = true, debug = is_mode("debug")}, system = false})
 add_requires("glslang 1.4.309+0", {configs = {debug = is_mode("debug")}, system = false})
-add_requires("slang vulkan-sdk-1.4.309", {configs = { shared = true, debug = is_mode("debug")}, system = false})
+add_requires("slang v2025.6.3", {configs = { shared = true, debug = is_mode("debug")}, system = false})
 add_requires("xxhash")
 
 target("vshadersystem")
