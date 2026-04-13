ea67a6c6feac8133392c976626475f9ec1cc3a74
Kexuan Zhang <sc23kz@leeds.ac.uk>
2026-04-13 17:47:19 +0100

    fix: static link spirv-cross to prevent errors.

--- source/vshadersystem/xmake.lua
+++ source/vshadersystem/xmake.lua
@@ -1,6 +1,6 @@
 -- note: spirv-cross & glslang must require the same vulkan sdk version
 -- we use 1.4.335
-add_requires("spirv-cross vulkan-sdk-1.4.335", {configs = { shared = true, debug = is_mode("debug")}, system = false})
+add_requires("spirv-cross vulkan-sdk-1.4.335", {configs = { debug = is_mode("debug")}, system = false})
 add_requires("glslang 1.4.335+0", {configs = {debug = is_mode("debug")}, system = false})
 add_requires("xxhash")
 
